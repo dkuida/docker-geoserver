b84ed4af8df7b56ae73b2314de9ba37a2590d6ee
dkuida <dan@kuida.org>
2018-02-26 10:27:09 +0100

    WIP
    tmp dirs for catalina

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env bash
-CONTAINER = "geoserver"
-docker build -t dkuida/$CONTAINER .
-docker stop $CONTAINER
-docker rm $CONTAINER
-docker run -p 8081:8080 --name=geo  -v /mnt/geoserver/data:/mnt/geoserver-data -v /mnt/geoserver/logs:/mnt/logs  -v /mnt/ortophoto:/mnt/ortophoto  --network=geo_nw  -d dkuida/$CONTAINER
+#!/bin/bash
+CONTAINER="geo"
+IMAGE="dkuida/geoserver"
+docker build -t ${IMAGE} .
+docker stop ${CONTAINER}
+docker rm ${CONTAINER}
+docker run -p 8081:8080 --name=geo -v /mnt/geoserver/data:/mnt/geoserver-data -v /mnt/geoserver/logs:/mnt/logs  -v /mnt/ortophoto:/mnt/ortophoto  --network=geo_nw -d ${IMAGE}
+
