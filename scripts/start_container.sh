ce05b7836d3b53acf4893b1c5459d9c03a655796
dkuida <dan@kuida.org>
2018-03-17 17:39:42 +0100

    cache location

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -8,7 +8,7 @@ docker stop ${CONTAINER}
 docker rm ${CONTAINER}
 docker run -p 8081:8080 --name=geo \
 -v /mnt/geoserver/data:/mnt/geoserver-data \
--v /mnt/cache:/mnt/geoserver-cache \
+-v /mnt/geoserver-cache:/mnt/cache \
 -v /mnt/geoserver/logs:/mnt/logs  \
 -v /mnt/ortophoto:/mnt/ortophoto  \
 --network=geo_nw -d ${IMAGE}
